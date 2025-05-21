7e0d36696506a2b8047b3331e7d80c845a74dd93
Trần Minh Phúc <50334644+TranMinhPhuc220420@users.noreply.github.com>
2025-05-21 21:22:45 +0700

    Revert not mongodb

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,15 @@
 
 namespace App\Models;
 
-use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Auth\Authenticatable;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class User extends Eloquent implements AuthenticatableContract
+class User extends Authenticatable
 {
-    use Authenticatable;
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
 
     protected $connection = 'mongodb';
 
